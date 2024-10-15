01f3df9286e9e62ee7dd5d585a15fd33495ea6c2
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2024-10-14 23:17:11 -0300

    chore: add flake.nix
    - add `cargo msrv` to justfile and add rust-version to `Cargo.toml`
    - add flake.nix with devShell with packages for development
    - add .envrc (direnv)

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Zapnote: Lightning-fast template-based note generator ";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bacon
+            cargo
+            cargo-msrv
+            cargo-nextest
+            clippy
+            just
+            nixfmt
+            rust-analyzer
+            rustc
+            rustfmt
+          ];
+        };
+      }
+    );
+}
--- justfile
+++ justfile
@@ -1,12 +1,15 @@
 default:
-    @just --list
+  @just --list
 
 test:
   cargo test
 
 fmt:
   cargo fmt --all
+  nixfmt -- *.nix
 
 lint:
   cargo fmt --all --check
   cargo clippy --all-features --all-targets -- -Dwarnings
+  cargo msrv verify
+  nixfmt --check -- *.nix
