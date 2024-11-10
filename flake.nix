9fdd9a449369b4231dd6ff6f117053531572d132
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2024-11-10 11:58:49 -0300

    feat(flake): add zapnote package to flake.nix
    Added zapnote package on `flake.nix` as `packages.default`, using naersk
    to build the package.

--- flake.nix
+++ flake.nix
@@ -4,13 +4,22 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, }:
+  outputs = { self, nixpkgs, flake-utils, naersk }:
 
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        naersk' = pkgs.callPackage naersk { };
       in {
+        # For `nix develop`:
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             bacon
@@ -23,6 +32,25 @@
             rustc
             rustfmt
           ];
+        };
+
+        packages = {
+          # For `nix build` `nix run`, & `nix profile install`:
+          default = naersk'.buildPackage {
+            pname = "zapnote";
+            version = "git";
+
+            src = ./.;
+            doCheck = true; # run `cargo test` on build
+
+            meta = with pkgs.lib; {
+              description = "Lightning-fast template-based note generator";
+              homepage = "https://github.com/lucasmartinsvieira/zapnote";
+              license = licenses.mit;
+              # maintainers = with maintainers; [ ];
+              mainProgram = "zn";
+            };
+          };
         };
       });
 }
